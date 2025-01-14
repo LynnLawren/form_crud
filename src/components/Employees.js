a03aea224bd101ac45090600422e8b700e46b0c0
LynnLawren <lynnmarylawrence@gmail.com>
2025-01-14 18:28:10 +0530

    modified

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+{
+    id:"1",
+    Name:"John Doe",
+    Age:"25"
+},
+{
+    id:"2",
+    Name:"Stephen Doe",
+    Age:"26"
+},
+{
+    id:"3",
+    Name:"Felix Doe",
+    Age:"28"
+}
+]
+
+export default Employees;
